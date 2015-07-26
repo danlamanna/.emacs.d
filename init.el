c807b75b37d93f7eeab82cc161352a31d663c3ea
Dan LaManna <me@danlamanna.com>
2015-07-26 19:10:30 -0400

    adds temporary helm configuration

--- init.el
+++ init.el
@@ -702,3 +702,33 @@
 ;; todo:
 ;; visual regexp/steroids
 ;; bookmarks
+;; autoparens and yas-expand issue
+;;
+
+
+(use-package-ensure helm
+  :demand t
+  :bind (("C-c h" . helm-command-prefix)
+         ("C-x b" . helm-mini)
+         ("C-x C-f" . helm-find-files)
+         ("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring))
+  :config (progn
+            (require 'helm-config)
+
+            (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+            (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+            (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+            (when (executable-find "curl")
+              (setq helm-google-suggest-use-curl-p t))
+
+            (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+                  helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+                  helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+                  helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+                  helm-ff-file-name-history-use-recentf t)
+
+
+
+            (helm-mode 1)))
