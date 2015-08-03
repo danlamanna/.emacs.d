7f67e0ed7b1ca2f7b68dfa4bba3b44a49954afe6
Dan LaManna <dan.lamanna@kitware.com>
2015-08-03 16:54:19 -0400

    add geojs settings/snippet

--- lisp/projects/geojs.el
+++ lisp/projects/geojs.el
@@ -1,5 +1,9 @@
 (setq geojs-project-dir "~/projects/geojs")
+(setq geojs-build-dir "~/projects/geojs-build")
 (setq geojs-venv "geojs-source")
+
+(add-to-list 'safe-local-variable-values
+             '(compile-command . (format "cd %s && ctest" geojs-build-dir)))
 
 (prodigy-define-service
   :name "GeoJS Grunt"
@@ -11,7 +15,9 @@
   :tags '(geojs grunt))
 
 (dir-locals-set-class-variables 'geojs
-                                `((python-mode . ((eval . ,(venv-workon geojs-venv))))))
+                                `((nil . ((compile-command .
+                                                           (format "cd %s && ctest" geojs-build-dir))))
+                                  (python-mode . ((eval . ,(venv-workon geojs-venv))))))
 
 (if (file-exists-p geojs-project-dir)
     (dir-locals-set-directory-class geojs-project-dir 'geojs))
