3da027666d39bea6f5ec51ef0d9857ef1023c7c6
Dan LaManna <dan.lamanna@kitware.com>
2015-08-04 15:30:43 -0400

    Add prodigy notifier, ignores to grep/ag

--- lisp/projects/geojs.el
+++ lisp/projects/geojs.el
@@ -1,3 +1,5 @@
+(require 'grep)
+
 (setq geojs-project-dir "~/projects/geojs")
 (setq geojs-build-dir "~/projects/geojs-build")
 (setq geojs-venv "geojs-source")
@@ -10,13 +12,21 @@
   :command "grunt"
   :cwd "/home/dan.lamanna/projects/geojs"
   :args '("watch")
+  :on-output (lambda (&rest args)
+               (let ((output (plist-get args :output))
+                     (service (plist-get args :service)))
+                 (when (s-matches? "Done, without errors" output)
+                   (message "GeoJS Built."))))
   :init (lambda ()
           (venv-workon geojs-venv))
   :tags '(geojs grunt))
 
 (dir-locals-set-class-variables 'geojs
                                 `((nil . ((compile-command .
-                                                           (format "cd %s && ctest" geojs-build-dir))))
+                                                           (format "cd %s && ctest" geojs-build-dir))
+                                          (grep-find-ignored-files . ,(append '("geo.js"
+                                                                                "geo.min.js")
+                                                                              grep-find-ignored-files))))
                                   (python-mode . ((eval . ,(venv-workon geojs-venv))))))
 
 (if (file-exists-p geojs-project-dir)
