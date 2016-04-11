68f0cd44d5cdfcb6f82b2f7c33c78f49d2deeed9
Dan LaManna <me@danlamanna.com>
2016-04-10 21:23:07 -0400

    Fix dired switches and navigation

--- init.el
+++ init.el
@@ -28,10 +28,28 @@
 (use-package dired
   :config (progn
             (custom-set-variables
+             '(dired-listing-switches "-Alh")
              '(dired-dwim-target t)
              '(dired-recursive-copies 'always)
              '(dired-recursive-deletes 'always)
-             '(wdired-allow-to-change-permissions t))))
+             '(wdired-allow-to-change-permissions t))
+
+            ;; Taken from whattheemacsd.com
+            (defun dired-back-to-top ()
+              (interactive)
+              (beginning-of-buffer)
+              (dired-next-line 2))
+
+            (define-key dired-mode-map
+              (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+
+            (defun dired-jump-to-bottom ()
+              (interactive)
+              (end-of-buffer)
+              (dired-next-line -1))
+
+            (define-key dired-mode-map
+              (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)))
 
 ;; I mostly just use this for font-locking keywords
 (use-package dockerfile-mode)
