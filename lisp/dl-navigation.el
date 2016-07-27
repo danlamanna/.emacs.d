910d32e0923a29f43b7838c9cd3fa4af199cc87f
Dan LaManna <dan.lamanna@kitware.com>
2016-07-27 11:41:05 -0400

    Ensure ace-jump-mode

--- lisp/dl-navigation.el
+++ lisp/dl-navigation.el
@@ -1,5 +1,6 @@
 (use-package ace-jump-mode
   :bind ("C-x SPC" . ace-jump-mode-pop-mark)
+  :ensure t
   :commands (ace-jump-word-mode
              ace-jump-char-mode
              ace-jump-line-mode))
