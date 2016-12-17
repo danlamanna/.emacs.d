89b323613f33b63278d7daf91fbdfa497e90df65
Dan LaManna <me@danlamanna.com>
2016-12-17 12:29:16 -0500

    Use a better keybind for delete-frame

--- lisp/dl-misc.el
+++ lisp/dl-misc.el
@@ -28,6 +28,7 @@ Leave point after open-quote."
 (global-set-key "\M-'" 'insert-quotations)
 (global-set-key "\M-\"" 'insert-quotes)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-x w") 'delete-frame)
 
 ;; backspace starts the isearch over
 (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
