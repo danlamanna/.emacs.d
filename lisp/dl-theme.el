f5e9c5fff35e37839c66b0097bc7ff3992c7d44f
Dan LaManna <me@danlamanna.com>
2017-04-03 22:01:59 -0400

    Update the theme to gruber-darker

--- lisp/dl-theme.el
+++ lisp/dl-theme.el
@@ -1,5 +1,10 @@
-(use-package color-theme-sanityinc-tomorrow
+(use-package gruber-darker-theme
   :ensure t
-  :init (load-theme 'sanityinc-tomorrow-night t))
+  :init (load-theme 'gruber-darker t)
+  :config (progn
+            (eval-after-load "magit"
+              (custom-set-faces
+               '(diff-refine-added ((t (:inherit diff-refine-change :background "#2d872d"))))
+               '(diff-refine-removed ((t (:inherit diff-refine-change :background "#872c2c"))))))))
 
 (provide 'dl-theme)
