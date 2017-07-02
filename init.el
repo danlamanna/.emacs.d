ea5f9c2f50d3feaabd624505da31ffcc78d36f09
Dan LaManna <me@danlamanna.com>
2017-07-01 23:02:38 -0400

    Switch to advising quit-window instead of Man-quit
    Man-quit was removed in emacs commit 506a97a58d with no warning that I
    could find.

--- init.el
+++ init.el
@@ -350,10 +350,10 @@ and it's name isn't in no-cleanup-filenames."
 (put 'narrow-to-region 'disabled nil)
 (setq inhibit-splash-screen t)
 
-(defadvice Man-quit(after maybe-close-frame activate)
+(defadvice quit-window(after maybe-close-frame activate)
   "`delete-frame-on-man-quit' is defined by an external script which
    launches an emacs frame for a specific man page. After being launched,
-   this allows the `Man-quit' function to close the frame."
+   this allows the `quit-window' function to close the frame."
   (if (frame-parameter (selected-frame) 'delete-frame-on-man-quit)
       (delete-frame)))
 
