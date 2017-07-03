d8e03d89609da8ae43988524a125a45e2d89ed5e
Dan LaManna <me@danlamanna.com>
2017-07-03 16:18:27 -0400

    wip

--- lisp/dl-python.el
+++ lisp/dl-python.el
@@ -14,13 +14,31 @@
              '(jedi:complete-on-dot t)
              '(jedi:tooltip-method nil)
              '(jedi:use-shortcuts t))))
+(use-package importmagic
+  :ensure t
+  :load-path "~/projects/importmagic.el"
+  :bind (:map python-mode-map
+              ("C-c i" . importmagic-fix-imports))
+  :init (add-hook 'python-mode-hook 'importmagic-mode))
 
 (use-package pycoverage
   :ensure t
   :commands pycoverage-mode)
 
 (use-package realgud
-  :ensure t)
+  :ensure t
+  :load-path "~/projects/realgud"
+  :bind ("C-c d" . dl-realgud-listen)
+  :config (progn
+            (defun dl-realgud-listen()
+              "Scan debugger to be listening using netcat, when done
+               call `realgud:pdb-remote' to connect."
+              (interactive)
+              (async-start (lambda()
+                             (save-window-excursion
+                               (shell-command "until nc -z localhost 4444; do sleep 1; done")))
+                           (lambda(&rest args)
+                             (realgud:pdb-remote "telnet localhost 4444"))))))
 
 (use-package sphinx-doc
   :ensure t
