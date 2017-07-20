3f20a2df29022e0cbfc748caf0bfeeb23c2617a8
Dan LaManna <me@danlamanna.com>
2017-07-20 08:15:41 -0400

    Add keybind for avy-pop-mark

--- lisp/dl-navigation.el
+++ lisp/dl-navigation.el
@@ -2,7 +2,7 @@
   :ensure t
   :bind (("M-g c" . avy-goto-char)
          ("M-g w" . avy-goto-word-or-subword-1)
-         ("M-g r" . avy-resume)))
+         ("M-g SPC" . avy-pop-mark)))
 
 (use-package avy-zap
   :ensure t
