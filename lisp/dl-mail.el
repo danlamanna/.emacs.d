039e4ab058460cc3494a9d3ede467713711e0a51
Dan LaManna <dan.lamanna@kitware.com>
2017-08-07 17:21:39 -0400

    wip

--- lisp/dl-mail.el
+++ lisp/dl-mail.el
@@ -2,6 +2,18 @@
   :ensure t
   :after (helm notmuch))
 
+(use-package hydra
+  :ensure t
+  :after (helm notmuch)
+  :config (progn
+            (defhydra hydra-mail (global-map "C-c m")
+              "mail"
+              ("s" helm-notmuch "search" :exit t)
+              ("r" (notmuch-tree "tag:unread -tag:ignore") "read" :exit t)
+              ("d" (notmuch-search "tag:draft") "drafts" :exit t)
+              ("c" compose-mail "compose" :exit t)
+              ("o" make-frame-command "new-frame" :color red))))
+
 (use-package notmuch
   :ensure t
   :bind (:map notmuch-search-mode-map
